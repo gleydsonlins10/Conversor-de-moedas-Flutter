bf05296f6875f6bef9bc32376ae50fbe6fe8a371
gleydson <gleydsonlins10@gmail.com>
2020-05-03 16:15:48 -0300

    Conversão dos dados

--- lib/main.dart
+++ lib/main.dart
@@ -40,16 +40,46 @@ class _HomeState extends State<Home> {
   double dolar;
   double euro;
 
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
   void _realChanged(String text) {
-      print(text);
+
+      if(text.isEmpty) {
+        _clearAll();
+        return;
+      }
+      double real = double.parse(text);
+      dolarController.text = (real/dolar).toStringAsFixed(2);
+      euroController.text = (real/euro).toStringAsFixed(2);
   }
 
   void _dolarChanged(String text) {
-    print(text);
+
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double dolar = double.parse(text);
+    dolarController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar /euro).toStringAsFixed(2);
   }
 
   void _euroChanged(String text) {
-    print(text);
+
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+
   }
 
   @override
@@ -118,6 +148,7 @@ Widget getWidgetTextField(String label, String prefixe, TextEditingController co
     style: TextStyle(
         color: Colors.amber, fontSize: 25.0
     ),
+    controller: controller,
     onChanged: fun,
     keyboardType: TextInputType.number,
   );
